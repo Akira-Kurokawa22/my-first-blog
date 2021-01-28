3b25ac1e1ab2eae369ae467d422b461fd1a39a09
Akira Kurokawa <ak.apple7777@gmail.com>
2021-01-28 23:03:49 +0900

    Changed the HTML for the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    return render(request, 'blog/post_list.html',{})
+    posts = Pos.objects.filter(Published_date__lte=timezone.now()).order_by('publishe_date')
+    return render(request, 'blog/post_list.html',{'posts':posts})
 # Create your views here.
